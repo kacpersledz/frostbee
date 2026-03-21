71f93d5c876f2feb57100308b61483808d4b40e7
kacper.sledz <casper.sledx@gmail.com>
2026-03-21 21:33:07 +0100

    Fix OTA poll interval time units

--- app/src/main.c
+++ app/src/main.c
@@ -608,15 +608,17 @@ static void confirm_running_image(void)
 
 static void set_ota_transfer_mode(bool enabled)
 {
-    if (enabled) {
-        zigbee_configure_sleepy_behavior(false);
-        zb_zdo_pim_set_long_poll_interval(OTA_KEEPALIVE_MS);
-        LOG_INF("OTA mode: FAST");
-    } else {
-        zigbee_configure_sleepy_behavior(true);
-        zb_zdo_pim_set_long_poll_interval(SED_KEEPALIVE_MS);
-        LOG_INF("OTA mode: SLEEPY");
-    }
+	if (enabled) {
+		zigbee_configure_sleepy_behavior(false);
+		zb_zdo_pim_set_long_poll_interval(
+			ZB_MILLISECONDS_TO_BEACON_INTERVAL(OTA_KEEPALIVE_MS));
+		LOG_INF("OTA mode: FAST");
+	} else {
+		zigbee_configure_sleepy_behavior(true);
+		zb_zdo_pim_set_long_poll_interval(
+			ZB_MILLISECONDS_TO_BEACON_INTERVAL(SED_KEEPALIVE_MS));
+		LOG_INF("OTA mode: SLEEPY");
+	}
 }
 
 #if IS_ENABLED(CONFIG_ZIGBEE_FOTA)
