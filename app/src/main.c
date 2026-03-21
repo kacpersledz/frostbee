be8de3b001b7346bcf3592c905c288f03f8d20b5
kacper.sledz <casper.sledx@gmail.com>
2026-03-21 21:49:06 +0100

    Improve OTA polling with turbo poll

--- app/src/main.c
+++ app/src/main.c
@@ -51,6 +51,7 @@ LOG_MODULE_REGISTER(frostbee, LOG_LEVEL_INF);
 #define REPORT_INTERVAL_S      15
 #define SED_KEEPALIVE_MS       3000
 #define OTA_KEEPALIVE_MS       100
+#define OTA_TURBO_POLL_MS      60000
 
 #define BUTTON_DEBOUNCE_MS         100
 #define BUTTON_SHORT_PRESS_MAX_MS  1000
@@ -610,14 +611,19 @@ static void set_ota_transfer_mode(bool enabled)
 {
 	if (enabled) {
 		zigbee_configure_sleepy_behavior(false);
-		zb_zdo_pim_set_long_poll_interval(
-			ZB_MILLISECONDS_TO_BEACON_INTERVAL(OTA_KEEPALIVE_MS));
-		LOG_INF("OTA mode: FAST");
+		zb_zdo_pim_set_long_poll_interval(OTA_KEEPALIVE_MS);
+		zb_zdo_pim_permit_turbo_poll(ZB_TRUE);
+		zb_zdo_pim_toggle_turbo_poll_retry_feature(ZB_TRUE);
+		zb_zdo_pim_start_turbo_poll_continuous(OTA_TURBO_POLL_MS);
+		LOG_INF("OTA mode: FAST (long poll %d ms, turbo poll on)",
+			OTA_KEEPALIVE_MS);
 	} else {
+		zb_zdo_pim_turbo_poll_continuous_leave(0);
+		zb_zdo_pim_toggle_turbo_poll_retry_feature(ZB_FALSE);
+		zb_zdo_pim_permit_turbo_poll(ZB_FALSE);
 		zigbee_configure_sleepy_behavior(true);
-		zb_zdo_pim_set_long_poll_interval(
-			ZB_MILLISECONDS_TO_BEACON_INTERVAL(SED_KEEPALIVE_MS));
-		LOG_INF("OTA mode: SLEEPY");
+		zb_zdo_pim_set_long_poll_interval(SED_KEEPALIVE_MS);
+		LOG_INF("OTA mode: SLEEPY (long poll %d ms)", SED_KEEPALIVE_MS);
 	}
 }
 
@@ -629,11 +635,17 @@ static void ota_evt_handler(const struct zigbee_fota_evt *evt)
 		if (!ota_in_progress) {
 			ota_in_progress = true;
 			set_ota_transfer_mode(true);
+		} else {
+			zb_zdo_pim_start_turbo_poll_continuous(OTA_TURBO_POLL_MS);
 		}
 		LOG_INF("OTA progress: %d%%", evt->dl.progress);
 		break;
 
 	case ZIGBEE_FOTA_EVT_FINISHED:
+		if (ota_in_progress) {
+			ota_in_progress = false;
+			set_ota_transfer_mode(false);
+		}
 		LOG_INF("OTA image ready, rebooting into MCUboot");
 		sys_reboot(SYS_REBOOT_COLD);
 		break;
